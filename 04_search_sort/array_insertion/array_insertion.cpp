a48243861becb3ee0935161ce14fb3b317d93532
vsueubaeva <sueubaeva.va@edu.spbstu.ru>
2022-12-15 22:19:44 +0300

    build: array_isnsertion.cpp search_position

--- 04_search_sort/array_insertion/array_insertion.cpp
+++ 04_search_sort/array_insertion/array_insertion.cpp
@@ -2,7 +2,9 @@
 #include <cassert>
 
 using namespace std;
+
 int* insert(int arr[], int x, int p, int n);
+int search_position (int arr[], int x, int n);
 
 int main(){
     int arr1[14] = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13 };
@@ -11,6 +13,11 @@ int main(){
     auto r1 = insert(arr1, 999, 6, 14);
     for (auto i = 0; i < 15; ++i)
         assert(r1[i] == rez1[i]);
+
+    int arr2[5] = {0, 1, 2, 3, 4};
+    assert(search_position(arr2, 2, 5) == 3);
+    assert(search_position(arr2, 7, 5) == 5);
+
 }
 
 int* insert (int arr[], int x, int p, int n){
@@ -20,4 +27,11 @@ int* insert (int arr[], int x, int p, int n){
     return arr;
 }
 
+int search_position (int arr[], int x, int n){
+    for (auto i = 0; i < n-1; ++i){
+        if (arr[i] >= x)
+        return i;
+    }
+    return n;
+}
 
