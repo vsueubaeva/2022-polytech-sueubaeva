a1b42c57b226c0784cd623fbcc14f5ef7c8b01bb
vsueubaeva <sueubaeva.va@edu.spbstu.ru>
2022-12-15 22:34:39 +0300

    feat: array_isnsertion.cpp insert_sorted

--- 04_search_sort/array_insertion/array_insertion.cpp
+++ 04_search_sort/array_insertion/array_insertion.cpp
@@ -5,18 +5,28 @@ using namespace std;
 
 int* insert(int arr[], int x, int p, int n);
 int search_position (int arr[], int x, int n);
+int* insert_sorted (int arr[], int x, int n);
 
 int main(){
     int arr1[14] = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13 };
-    int rez1[15] = { 0, 1, 2, 3, 4, 5, 999, 7, 8, 9, 10, 11, 12, 13 };
+    int rez1[15] = { 0, 1, 2, 3, 4, 5, 999, 6, 7, 8, 9, 10, 11, 12, 13};
 
     auto r1 = insert(arr1, 999, 6, 14);
     for (auto i = 0; i < 15; ++i)
-        assert(r1[i] == rez1[i]);
+    assert(r1[i] == rez1[i]);
 
     int arr2[5] = {0, 1, 2, 3, 4};
     assert(search_position(arr2, 2, 5) == 3);
     assert(search_position(arr2, 7, 5) == 5);
+
+    int arr3[14] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13};
+    int rez3[15] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 8, 9, 10, 11, 12, 13};
+
+    auto r3 = insert_sorted(arr3, 8, 14);
+    for (auto i = 0; i < 15; i++)
+    assert(r3[i] == rez3[i]);
+
+    return 0;
 
 }
 
@@ -33,5 +43,10 @@ int search_position (int arr[], int x, int n){
         return i;
     }
     return n;
+}
+
+int* insert_sorted (int arr[], int x, int n){
+    int p = search_position(arr, x, n);
+    return insert(arr, x, p, n);
 }
 
